931bc24c5ca8d5f32cb5c42cefd2226298e205dd
Justin Soberano <jsoberano2002@gmail.com>
2025-01-06 15:25:52 -0600

    aaaaaa

--- src/App.js
+++ src/App.js
@@ -5,7 +5,39 @@ import { AnimatedPointLight } from './components/lighting/AnimatedPointLight';
 import { PostProcessingEffects } from './components/effects/PostProcessingEffects';
 import { useResponsiveCamera } from './components/hooks/useResponsiveCamera';
 
+import {useEffect, useState} from 'react';
+
+function getDeviceType() {
+  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  if (/android/i.test(userAgent)) {
+    return 'Android';
+  }
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    return 'iOS';
+  }
+  if (/windows phone/i.test(userAgent)) {
+    return 'Windows Phone';
+  }
+  if (/mobile/i.test(userAgent)) {
+    return 'Mobile';
+  }
+  return 'Desktop';
+}
+
 function App() {
+  const [dpr, setDpr] = useState(1);
+  const [device, setDevice] = useState('Desktop');
+
+  useEffect(() => {
+    const deviceType = getDeviceType();
+    setDevice(deviceType);
+    if (deviceType === 'Android' || deviceType === 'iOS' || deviceType === 'Mobile') {
+      setDpr(2);
+    } else {
+      setDpr(1.4); //sharper imaeg
+    }
+  }, []);
+
   const cameraPosition = useResponsiveCamera();
 
   return (
@@ -13,7 +45,7 @@ function App() {
       <div className="box">
         WORK IN PROGRESS
       </div>
-      <Canvas dpr={1} style={{ backgroundColor: 'black' }} shadows camera={{ position: cameraPosition }}>
+      <Canvas dpr={dpr} style={{ backgroundColor: 'black' }} shadows camera={{ position: cameraPosition }}>
         <AnimatedPointLight />
         <FirstName />
         <PostProcessingEffects />
